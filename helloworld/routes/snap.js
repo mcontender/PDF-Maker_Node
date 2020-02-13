30eeccd9472cde925ed61f865b2548955d5a1eea
Michael Valeron <mcontender@gmail.com>
2020-02-13 15:09:49 -0500

    Updates
    Updated SS folder structure, made the folder structure create them on the fly if they are not available

--- helloworld/routes/snap.js
+++ helloworld/routes/snap.js
@@ -1,17 +1,25 @@
 var express = require('express');
 const puppeteer = require('puppeteer');
+const fs = require('fs')
 var router = express.Router();
 
 function wait(ms) {
     return new Promise(resolve => setTimeout(() => resolve(), ms));
 }
 
-async function SShot(url) {
+async function SShot(res, next, url) {
     let browser = await puppeteer.launch({ headless: false });
     let page = await browser.newPage();
     let fullPage = false;
-    console.log(url);
+    console.log('Page URL: ' + url);
     try {
+        var date = new Date();
+        var dir = './public/screenshots/' + date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + date.getDate();
+        console.log('Directory: ' + dir);
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+
         await page.setViewport({ width: 1920, height: 1080 });
         await page.goto(url, { waitUntil: 'networkidle2' }); // make sure all JS is loaded and all Ajax requests are done.
         await page.emulateMedia('screen'); // or screen
@@ -24,42 +32,41 @@ async function SShot(url) {
 
             // -----------------------------
             // Lazy Load Page first
-            // const bodyHandle = await page.$('.gcss-page');
-            // const { height } = await bodyHandle.boundingBox();
-            // console.log(height);
-            // if (height < 1080) {
-            //     bodyHandle = await page.$('.globa-wrapper');
-            // }
-            // await bodyHandle.dispose();
+            const bodyHandle = await page.$('.gcss-page');
+            const { height } = await bodyHandle.boundingBox();
+            console.log('Page length: ' + height);
+            if (height < 1080) {
+                bodyHandle = await page.$('.globa-wrapper');
+            }
+            await bodyHandle.dispose();
 
-            // // Scroll one viewport at a time, pausing to let content load
-            // let viewportIncr = 0;
-            // let screenNum = 0;
-            // await page.screenshot({ path: './public/screenshots/scrolling/screen_' + screenNum + '.png' });
-            // await wait(2000);
-            // const viewportHeight = page.viewport().height;
-            // console.log('lazy load: starting');
+            // Scroll one viewport at a time, pausing to let content load
+            let viewportIncr = 0;
+            let screenNum = 0;
+            await page.screenshot({ path: './public/screenshots/scrolling/screen_' + screenNum + '.png' });
+            await wait(2000);
+            const viewportHeight = page.viewport().height;
+            console.log('Lazy Load: starting');
 
-            // while (viewportIncr + viewportHeight < height) {
-            //     screenNum++;
-            //     await page.evaluate(_viewportHeight => {
-            //         window.scrollBy(0, _viewportHeight);
-            //     }, viewportHeight);
-            //     await wait(2000);
-            //     viewportIncr = viewportIncr + viewportHeight;
-            //     await page.screenshot({ path: './public/screenshots/scrolling/screen_' + screenNum + '.png' });
-            //     console.log('lazy load: going to the bottom');
-            // }
+            while (viewportIncr + viewportHeight < height) {
+                screenNum++;
+                await page.evaluate(_viewportHeight => {
+                    window.scrollBy(0, _viewportHeight);
+                }, viewportHeight);
+                await wait(2000);
+                viewportIncr = viewportIncr + viewportHeight;
+                await page.screenshot({ path: './public/screenshots/scrolling/screen_' + screenNum + '.png' });
+            }
+            console.log('Lazy Load: end');
             // Lazy Load END -----------------------------
 
-            console.log('children elements: start finding')
+            console.log('Children Elements: start finding')
                 // execute standard javascript in the context of the page.
                 // const stories = await page.evaluate('.parsys', sections => { return $('.parsys').children });
             const sections = await page.evaluate(() => {
                     var results = Array.from(document.querySelectorAll('.parsys')[0].children)
                     var childrenOfChildrenID = Array(results.length);
-                    console.log('children elements: loop for more')
-                    console.log(results);
+                    console.log('Children Elements: loop for more')
                     for (var x = 0; x < results.length - 1; x++) {
                         // console.log('children elements: ele = ' + document.querySelectorAll('.' + results[x].className.split(' ')[0])[0].children[0]);
                         if (document.querySelectorAll('.' + results[x].className.split(' ')[0])[0].children[0].id != '') {
@@ -67,10 +74,7 @@ async function SShot(url) {
                             results[x].childs = childrenOfChildrenID[x];
                         }
                     }
-                    console.log(childrenOfChildrenID);
                     return results.map((result) => {
-                        // console.log(result);
-                        // return result;
                         return {
                             class: result.className,
                             id: result.id,
@@ -90,6 +94,7 @@ async function SShot(url) {
             element = await page.$('.hero');
             await element.screenshot({ path: './public/screenshots/sections/section_0.png' });
             for (var i = 0; i < sections.length; i++) {
+                console.log(sections[i]);
                 // console.log('Sections: Childs ' + i + ' (' + sections[i].childs + ')');
                 // console.log(sections[i].class.toString().split(' ')[0]);
                 if (!sections[i].class.includes('open-container')) {
@@ -112,7 +117,9 @@ async function SShot(url) {
                         //     clip: { x: sections[i].offLeft, y: sections[i].offTop, width: sections[i].offWidth, height: sections[i].offHeight }
                         // });
                     }
-                    await page.screenshot({ path: './public/screenshots/sections/section_' + (i + 1) + '.png' });
+
+                    var fileName = dir + '/section_' + (i + 1) + '.png';
+                    await page.screenshot({ path: fileName });
                     // TODO: After first SS is taken then look for disclaimer in view
 
                     // TODO: After disclaimer SS is taken then look for Arrow/tabs
@@ -128,36 +135,38 @@ async function SShot(url) {
         // ----------------------
 
         // ----------------------
-        // LAZY LOAD
-        if (fullPage) {
-            const bodyHandle = await page.$('.globalWrapper');
-            const { height } = await bodyHandle.boundingBox();
-            await bodyHandle.dispose();
-            await page.screenshot({ path: './public/screenshots/test.jpg', type: 'jpeg', fullPage: false });
+        // LAZY LOAD - Deprecated, it doesn't do what we need correctly
+        // if (fullPage) {
+        //     const bodyHandle = await page.$('.globalWrapper');
+        //     const { height } = await bodyHandle.boundingBox();
+        //     await bodyHandle.dispose();
+        //     await page.screenshot({ path: './public/screenshots/test.jpg', type: 'jpeg', fullPage: false });
 
-            // Scroll one viewport at a time, pausing to let content load
-            // const viewportHeight = page.viewport().height;
-            let viewportIncr = 0;
-            let screenNum = 0;
-            await page.screenshot({ path: './public/screenshots/scrolling/screen_' + screenNum + '.png' });
-            await wait(2000);
-            const viewportHeight = page.viewport().height;
-            console.log(viewportHeight);
+        //     // Scroll one viewport at a time, pausing to let content load
+        //     // const viewportHeight = page.viewport().height;
+        //     let viewportIncr = 0;
+        //     let screenNum = 0;
+        //     await page.screenshot({ path: './public/screenshots/scrolling/screen_' + screenNum + '.png' });
+        //     await wait(2000);
+        //     const viewportHeight = page.viewport().height;
+        //     console.log(viewportHeight);
 
-            while (viewportIncr + viewportHeight < height) {
-                screenNum++;
-                await page.evaluate(_viewportHeight => {
-                    window.scrollBy(0, _viewportHeight);
-                }, viewportHeight);
-                await wait(2000);
-                viewportIncr = viewportIncr + viewportHeight;
-                await page.screenshot({ path: './public/screenshots/screen_' + screenNum + '.png' });
-            }
-        }
+        //     while (viewportIncr + viewportHeight < height) {
+        //         screenNum++;
+        //         await page.evaluate(_viewportHeight => {
+        //             window.scrollBy(0, _viewportHeight);
+        //         }, viewportHeight);
+        //         await wait(2000);
+        //         viewportIncr = viewportIncr + viewportHeight;
+        //         await page.screenshot({ path: './public/screenshots/screen_' + screenNum + '.png' });
+        //     }
+        // }
         // ----------------------
 
         await page.close();
         await browser.close();
+
+        res.send('Screenshot taken for: ' + url); //req.query.url);
 
     } catch (err) {
         console.log(err);
@@ -166,8 +175,8 @@ async function SShot(url) {
 
 router.get('/', function(req, res, next) {
     console.log('[LOG] Submitted URL: ' + req.query.url);
-    SShot(req.query.url);
-    res.send('Screenshot taken for: ' + req.query.url); //req.query.url);
+    SShot(res, next, req.query.url);
+    // res.send('Screenshot taken for: ' + req.query.url); //req.query.url);
 });
 
 module.exports = router;
