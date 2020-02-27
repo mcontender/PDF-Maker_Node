7d99559b6c589996733f9c400a027c0a54549a5f
Jimmy Satizabal <jimmy.satizabal@publicissapient.com>
2020-02-27 16:28:35 -0500

    - brand password - check URL for preview,test,uat string if true, enable brand password inout field -  add css transition because i know how much Mike Valeron and creatives love them....

--- helloworld/public/javascripts/openBrowser.js
+++ helloworld/public/javascripts/openBrowser.js
@@ -13,4 +13,22 @@ $(document).ready(function() {
             $('#response').html(response); // show the list
         });
     });
+
+    $("#url").on("input", function(){
+        // Print entered value in a div box
+        var value = $(this).val();
+        console.log(value);
+        if (value.indexOf('preview') >= 0 || value.indexOf('test') >= 0 || value.indexOf('uat') >= 0) {
+            $('#brandPsw').removeAttr('disabled');
+            $('.brand').removeClass('disabled');
+        } else {
+            $('#brandPsw').attr('disabled', 'true');
+            $('.brand').addClass('disabled');
+       }
+        //$("#result").text($(this).val());
+    })
+    
+    
+   
+
 });
